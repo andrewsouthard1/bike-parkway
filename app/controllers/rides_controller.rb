a4265092041da89c9b39334c4cd3d17e618a25f3
Andrew <andrewsouthard111@gmail.com>
2017-04-09 15:44:39 -0400

    Add user id to rides

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,10 +4,14 @@ class RidesController < ApplicationController
   end
 
   def create
-    @ride = Ride.create(
+    @ride = Ride.new(
       starting_location: params[:starting_location],
       ending_location: params[:ending_location]
     )
+    if current_user
+      @ride.user_id = current_user.id
+    end
+    @ride.save
     redirect_to "/rides/#{@ride.id}"
   end
 
