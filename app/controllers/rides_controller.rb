881ab921fbfbfd3503442fa9555ef9881ac24382
Andrew <andrewsouthard111@gmail.com>
2017-04-12 19:19:36 -0400

    Basic markers for start, end location, set middle input as the center

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -18,6 +18,13 @@ class RidesController < ApplicationController
   def show
     @ride = Ride.find(params[:id])
     @ride_directions = Unirest.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{@ride.starting_location}&destination=#{@ride.ending_location}&mode=bicycling&key=#{ENV['API_KEY']}").body
+    @start_latitude = @ride_directions["routes"][0]["legs"][0]["start_location"]["lat"]
+    @start_longitude = @ride_directions["routes"][0]["legs"][0]["start_location"]["lng"]
+    @end_latitude = @ride_directions["routes"][0]["legs"][0]["end_location"]["lat"]
+    @end_longitude = @ride_directions["routes"][0]["legs"][0]["end_location"]["lng"]
+    
+
+
     render 'show.html.erb'
   end
 end
