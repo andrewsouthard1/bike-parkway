0633ae149fc225f281426b898daac3f642a1a5cb
Andrew <andrewsouthard111@gmail.com>
2017-05-06 12:22:33 -0400

    Centered map, app

--- app/assets/javascripts/rides.js
+++ app/assets/javascripts/rides.js
@@ -6,7 +6,7 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     mapTypeControl: false,
     center: {lat: 40.730610, lng: -73.935242},
-    zoom: 10
+    zoom: 13
   });
 
   new AutocompleteDirectionsHandler(map);
