526a3ff3b28e7512ff64ffe1538aa3c7d6205442
Andrew <andrewsouthard111@gmail.com>
2017-05-14 12:30:36 -0400

    Styled users page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
   has_many :rides
+
+  def friend?(input_id)
+    friendships = Friendship.all.where(user_id: self.id)
+    friendships.each do |friendship|
+      if friendship.friend_id == input_id
+        return true
+      end
+    end
+    false
+  end
 end
